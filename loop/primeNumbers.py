186ea3e3589c80f03f6ad58505d423e0b597758d
yamunaAsokan <59139676+yamunaAsokan@users.noreply.github.com>
2019-12-26 08:50:36 +0530

    Create primeNumbers.py

---
+++ loop/primeNumbers.py
@@ -0,0 +1,8 @@
+n=int(input('enter the n th velue'))
+for i in range(1,n+1):
+    if(i>1):
+        for a in range(2,i):
+            if(i%a)==0:
+                break
+        else:
+            print(i)    
